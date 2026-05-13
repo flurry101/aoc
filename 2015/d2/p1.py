7d01e1350c475408b41589b68bc76a99f707f41d
flurry101 <sravya.sm1491@gmail.com>
2026-05-13 16:32:50 +0000

    2015d2

--- 2015/d2/p1.py
+++ 2015/d2/p1.py
@@ -1,2 +1,7 @@
 def solve(input):
-    pass
+    s=0
+    for line in input.splitlines():
+        l,w,h=[int(i) for i in line.split('x')]
+        sa=2*(l*w+w*h+h*l)+ min(l*w,w*h,l*h)
+        s+=sa
+    print(s)
--- 2015/d2/p2.py
+++ 2015/d2/p2.py
@@ -1,2 +1,8 @@
 def solve(input):
-    pass
+    s=0
+    for line in input.splitlines():
+        l,w,h=[int(i) for i in line.split('x')]
+        dims=sorted([l,w,h])
+        ribbon=2*(dims[0]+dims[1])+l*w*h
+        s+=ribbon
+    print(s)
