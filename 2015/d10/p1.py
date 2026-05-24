51a42f7a9d16e9c361b026c1b7fc97681c60742f
flurry101 <sravya.sm1491@gmail.com>
2026-05-24 18:39:04 +0000

    2015d10

---
+++ 2015/d10/p1.py
@@ -0,0 +1,12 @@
+def solve(input):
+    for _ in range(40):
+        cur,out,c = input[0],[],1
+        for dgt in input[1:]:
+            if dgt==cur:
+                c+=1
+            else:
+                out.append(str(c)+cur)
+                cur=dgt
+                c=1
+        input="".join(out)+str(c) + cur
+    print(len(input))
